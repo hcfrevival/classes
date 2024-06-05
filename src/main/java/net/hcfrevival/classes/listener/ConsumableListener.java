5fd3df85ecaf97e408ba9be1ad8f71af35be2919
j-rockwell <john@llewkcor.com>
2024-06-05 13:41:44 -0700

    main: port onEffectExpire fix for class consumables

--- src/main/java/net/hcfrevival/classes/listener/ConsumableListener.java
+++ src/main/java/net/hcfrevival/classes/listener/ConsumableListener.java
@@ -2,18 +2,25 @@ package net.hcfrevival.classes.listener;
 
 import gg.hcfactions.libs.base.util.Strings;
 import gg.hcfactions.libs.base.util.Time;
+import gg.hcfactions.libs.bukkit.scheduler.Scheduler;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import net.hcfrevival.classes.ClassService;
+import net.hcfrevival.classes.types.IClass;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.entity.EntityPotionEffectEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.potion.PotionEffect;
+
+import java.util.Optional;
 
 @AllArgsConstructor
 public final class ConsumableListener implements Listener {
@@ -46,6 +53,41 @@ public final class ConsumableListener implements Listener {
             }
 
             consumable.consume(player, item);
+        });
+    }
+
+    @EventHandler (priority = EventPriority.HIGHEST)
+    public void onEffectExpire(EntityPotionEffectEvent event) {
+        final PotionEffect effect = event.getOldEffect();
+
+        if (effect == null) {
+            return;
+        }
+
+        if (!(event.getEntity() instanceof final Player player)) {
+            return;
+        }
+
+        final EntityPotionEffectEvent.Action action = event.getAction();
+
+        if (!action.equals(EntityPotionEffectEvent.Action.REMOVED) && !action.equals(EntityPotionEffectEvent.Action.CLEARED)) {
+            return;
+        }
+
+        service.getCurrentClass(player).ifPresent(playerClass -> {
+            if (!playerClass.getConfig().getPassiveEffects().containsKey(effect.getType())) {
+                return;
+            }
+
+            final int amplifier = playerClass.getConfig().getPassiveEffects().get(effect.getType());
+
+            new Scheduler(service.getPlugin()).sync(() -> {
+                Optional<IClass> futureClassQuery = service.getCurrentClass(player);
+
+                if (futureClassQuery.isPresent() && futureClassQuery.get() == playerClass) {
+                    player.addPotionEffect(new PotionEffect(effect.getType(), PotionEffect.INFINITE_DURATION, amplifier));
+                }
+            }).delay(1L).run();
         });
     }
 }
